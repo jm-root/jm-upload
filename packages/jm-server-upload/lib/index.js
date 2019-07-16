9206edf354440a32614175879f06b3e4806a56c6
jamma <jeff@jamma.cn>
2019-07-16 11:57:50 +0800

    file ext

--- packages/jm-server-upload/lib/index.js
+++ packages/jm-server-upload/lib/index.js
@@ -1,9 +1,22 @@
 const express = require('express')
 const multer = require('multer')
+const path = require('path')
+const { ObjectId } = require('bson')
+
+function genId () {
+  return new ObjectId().toString()
+}
 
 module.exports = function (opts = {}) {
+  const defaultStorage = multer.diskStorage({
+    filename: function (req, file, cb) {
+      cb(null, genId() + path.extname(file.originalname))
+    }
+  })
+
   const { upload_dir: uploadDir = process.cwd() + '/uploads', upload_prefix: uploadPrefix = '', prefix = '/', fields } = opts
   opts.dest = uploadDir + uploadPrefix
+  opts.storage || (opts.storage = defaultStorage)
 
   const router = express.Router()
   const upload = multer(opts)
